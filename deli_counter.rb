58336fe3301a3ea6cfa6c850f6924d1edfdbc854
James Batsell <jamesbatsell05@gmail.com>
2020-01-28 23:46:04 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(deli)
+  if deli.length = 0
+    puts "The line is currently empty"
+  else
+    str = "the line is  currently:"
+    deli.each.with_index(1) do |person, i|
+    str += "#{i}. #{person}"
+    end
+    puts str
+  end
+end
+
+
+
+def take_a_number(deli, person)
+  deli << person
+  puts "welcome, #{person}. 
+
+end
+
+def now_serving()
+  .index(person)} in line."
